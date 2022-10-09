965d330bbfe8b13c2f5a5f0ade46527867eea100
Nisseboy <nils@tjarnlunds.se>
2022-10-09 16:38:50 +0200

    sex

--- components/Window.js
+++ components/Window.js
@@ -1,10 +1,17 @@
-import styles from './Window.module.css';
+import styles from '../styles/Window.module.css';
 
 export default function Window({ title }) {
   return (
   <div className={styles.window}>
     <img className={styles.windowImage} src="/songs/Adele - Rolling in the Deep (Official Music Video).jpg"></img>
     <div className={styles.windowText}>{title}</div>
-    <div className={styles.windowBar}>{title}</div>
+    <div className={styles.windowBar}>
+      <div className={styles.windowPlay}>▶</div>
+      <div className={styles.windowProgress}>
+        <div className={styles.windowProgressDotBefore}></div>
+        <div className={styles.windowProgressDot}></div>
+        <div className={styles.windowProgressDotAfter}></div>
+      </div>
+    </div>
   </div>)
 }
