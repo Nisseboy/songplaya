3da137e0997999f5d12715c41b013a58d685f03d
Nisseboy <nils@tjarnlunds.se>
2022-10-09 16:19:53 +0200

    first

---
+++ components/Window.js
@@ -0,0 +1,10 @@
+import styles from './Window.module.css';
+
+export default function Window({ title }) {
+  return (
+  <div className={styles.window}>
+    <img className={styles.windowImage} src="/songs/Adele - Rolling in the Deep (Official Music Video).jpg"></img>
+    <div className={styles.windowText}>{title}</div>
+    <div className={styles.windowBar}>{title}</div>
+  </div>)
+}
